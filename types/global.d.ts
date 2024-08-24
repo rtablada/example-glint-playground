1b9a1d995cd6253c4158218cf919a813b2ad67e4
Ryan Tablada <ryan.tablada@gmail.com>
2024-08-23 19:51:37 -0500

    feat: show error when js backing class

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,7 @@
 import '@glint/environment-ember-loose';
-import { ComponentLike } from '@glint/template';
+
 import type EmberPageTitleTemplateRegistry from 'ember-page-title/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry extends EmberPageTitleTemplateRegistry {
-    WelcomePage: ComponentLike;
-  }
+  export default interface Registry extends EmberPageTitleTemplateRegistry {}
 }
